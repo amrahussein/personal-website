443bc3053af279cca4f91c374a8e93f0ac2a71f3
Amro Abdelkamel <amromoorie@gmail.com>
2021-10-21 05:02:39 +0200

    Updates

---
+++ _next/static/PkEICAwRCZv8q_5kpsedx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3db6c3e389e8ea1fa4f6.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
