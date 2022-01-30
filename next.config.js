ac42cf33b66d13ce01924d0c37ab1e83feb8672e
Amro Abdelkamel <amromoorie@gmail.com>
2022-01-30 20:11:25 +0200

    Config: push package-lock json for github actions (ci)

--- next.config.js
+++ next.config.js
@@ -1,37 +1,36 @@
 module.exports = {
-  
-//   // reactStrictMode: true,
-//   // env: {
-//   //   PUBLIC_URL: 'https://amromoorie.github.io/personal-website',
-//   //   assetPrefix: './',
-//   // },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          // {
-          //   key: 'Content-Security-Policy',
-          //   value:
-          //     "default-src 'self'; font-src 'self' fonts.gstatic.com/; style-src 'self' fonts.googleapis.com/ 'unsafe-inline';",
-          //     // "default-src 'self'; font-src 'self' https://fonts.gstatic.com/; style-src 'self' https://fonts.googleapis.com/ 'unsafe-inline';",
-          // },
-          {
-            key: 'Content-Security-Policy',
-            value:
-              "default-src 'self';font-src fonts.gstatic.com;style-src 'self' fonts.googleapis.com",
-              // "default-src 'self'; font-src 'self' https://fonts.gstatic.com/; style-src 'self' https://fonts.googleapis.com/ 'unsafe-inline';",
-          },
-          // {
-          //   key: 'Content-Security-Policy',
-          //   value:
-          //     "font-src 'self' data:; img-src 'self' data:; default-src 'self' http://121.0.0:3001/",
-          //     // "default-src 'self'; font-src 'self' https://fonts.gstatic.com/; style-src 'self' https://fonts.googleapis.com/ 'unsafe-inline';",
-          // },
-        ],
-      },
-    ];
+  reactStrictMode: false,
+  env: {
+    PUBLIC_URL: 'https://amromoorie.github.io/personal-website',
+    assetPrefix: './',
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)',
+  //       headers: [
+  //         // {
+  //         //   key: 'Content-Security-Policy',
+  //         //   value:
+  //         //     "default-src 'self'; font-src 'self' fonts.gstatic.com/; style-src 'self' fonts.googleapis.com/ 'unsafe-inline';",
+  //         //     // "default-src 'self'; font-src 'self' https://fonts.gstatic.com/; style-src 'self' https://fonts.googleapis.com/ 'unsafe-inline';",
+  //         // },
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value:
+  //             "default-src 'self';font-src fonts.gstatic.com;style-src 'self' fonts.googleapis.com",
+  //             // "default-src 'self'; font-src 'self' https://fonts.gstatic.com/; style-src 'self' https://fonts.googleapis.com/ 'unsafe-inline';",
+  //         },
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value:
+  //             "font-src 'self' data:; img-src 'self' data:; default-src 'self' http://121.0.0:3001/",
+  //             // "default-src 'self'; font-src 'self' https://fonts.gstatic.com/; style-src 'self' https://fonts.googleapis.com/ 'unsafe-inline';",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 
 
 };
