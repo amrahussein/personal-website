783f40fd09477b2fb3a0a86f6dd774cbe4086df4
Amro Abdelkamel <amromoorie@gmail.com>
2022-07-08 23:59:37 +0200

    Refactor(brand button): - refactor brand button into a micro component

---
+++ components/micros/buttons/ButtonBrandBlue.jsx
@@ -0,0 +1,7 @@
+export default function ButtonBrandBlue({ children }) {
+  return (
+    <h3 className='mt-2 px-4 py-1 ml-6 tracking-widest float-right bg-accent rounded-full transition duration-200 italic text-secodary hover:text-cool border-2 border-b-primary hover:border-b-cool border-x-0 border-t-0 '>
+      {children}
+    </h3>
+  )
+}
