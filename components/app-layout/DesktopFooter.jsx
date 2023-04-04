7912c49848a5bc51ef287ac512b46fb01fcdf25e
Amr Abdelkamel <amromoorie@gmail.com>
2023-04-04 11:53:15 +0200

    feat (footer): - desktop footer to be fixed in the layout

--- components/app-layout/DesktopFooter.jsx
+++ components/app-layout/DesktopFooter.jsx
@@ -1,10 +1,9 @@
-import Copyright from "../micros/Copyright";
-import GoSocials from "../micros/GoSocials";
-
+import Copyright from '../micros/Copyright';
+import GoSocials from '../micros/GoSocials';
 
 export default function DesktopFooter() {
   return (
-    <footer className='mt-44 px-10 py-10 flex justify-between items-center absolute bottom-1 inset-x-0 border-solid border-x-0 border-4 border-b-0 border-accent-700 '>
+    <footer className='fixed inset-x-0 bottom-0 flex items-center justify-between border-4 border-x-0 border-b-0 border-solid border-accent bg-white px-10 py-4'>
       <Copyright />
 
       <GoSocials
@@ -13,5 +12,5 @@ export default function DesktopFooter() {
         linkStyle=' w-6 h-6  rounded-full hover:scale-125 hover:text-important hover:rotate-[5deg]'
       />
     </footer>
-  )
+  );
 }
