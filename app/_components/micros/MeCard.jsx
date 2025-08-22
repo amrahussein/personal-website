dca4187f892d458f527fdb320924c0f090b1426f
Amr Hussein <amrahussein@hotmail.com>
2025-08-22 22:01:08 +0300

    rm link for me card

--- app/_components/micros/MeCard.jsx
+++ app/_components/micros/MeCard.jsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { useState } from 'react';
 import useIsMobile from '../../../context/IsMobile.context';
 import basePath from '../../utils/basePath';
@@ -53,18 +52,14 @@ export default function MeCard({ w = 300, h = 300 }) {
         </figcaption>
         <div className='flex flex-col items-center justify-center py-6 text-gray-500 lg:py-8 '>
           <h1 className='md:textd-lg sm:text-md pb-3 text-sm font-extrabold leading-3'>
-            <Link
-              aria-label=''
-              target='_blank'
-              rel=''
-            >
+            <span>
               <span className='text-primary hover:text-secondary dark:text-important dark:hover:text-primary'>
                 @
               </span>{' '}
               <strong className='font-extrabold text-secondary underline opacity-70 hover:text-primary dark:text-slate-50 dark:hover:text-slate-300'>
                 amrahussein
               </strong>
-            </Link>
+            </span>
           </h1>
           <p className='mb-[0.2rem] text-center text-sm text-secondary dark:text-slate-50 sm:px-6 sm:leading-5'>
             <em>Passionate Developer</em>
