40831b0a523d399ab8522dd6fe0c1a4694a46fee
Amr Hussein <amrahussein@hotmail.com>
2025-08-22 21:00:03 +0300

    update domain, config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@
  * @type {import('next').NextConfig}
  */
 const isGithubPages = process.env.NODE_ENV === 'production';
+const isCustomDomain = process.env.GITHUB_PAGES_CUSTOM_DOMAIN === 'true';
 const repoName = 'personal-website';
 
 module.exports = {
   output: 'export',
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  basePath: (isGithubPages && !isCustomDomain) ? `/${repoName}` : '',
+  assetPrefix: (isGithubPages && !isCustomDomain) ? `/${repoName}/` : '',
   // images: { path: isGithubPages ? `/${repoName}/_next/image` : '/_next/image' }
 };
